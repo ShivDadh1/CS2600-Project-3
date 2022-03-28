e5f5b561e58f218a10b8ed9e8ed753e91fc4669f
ShivDadh1 <98553193+ShivDadh1@users.noreply.github.com>
2022-03-27 22:15:02 -0700

    main.c file
    main.c file

---
+++ main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "address_book.h"
+#include "address_book_fops.h"
+#include "address_book_menu.h"
+#include "address_book_menu.c"
+#include "address_book_fops.c"
+
+int main(void)
+{
+	printf("Hello World");
+	AddressBook address_book;
+	Status ret;
+
+	/* Load the file from .csv file if any */
+	ret = load_file(&address_book);
+
+	if (ret == e_success)
+	{
+		/* Show all the available menu */
+		ret = menu(&address_book);
+
+		if (ret == e_success)
+		{
+			/* Save the entries */
+			save_prompt(&address_book);
+		}
+	}
+
+	return 0;
+}
